dfeb64634d363937cb012a1be4b0f75ea98adcad
Ismail Courr <contact@ismailcourr.ml>
2022-08-05 20:28:01 +0100

    Add foodApp preview gif

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ const projectPop = [
   {
     name: 'Food App',
     desc: 'This is a food and recipe app built using RoR, you can add food and create recipes with the food from the database. This user can log in to the app and store the recipes as private or share them publicly with all the users of the app. it also has a shopping list so the user can track foods that are not available anymore',
-    featuredImage: 'img/projects/foodApp/preview0.png',
+    featuredImage: 'img/projects/foodApp/foodApp.gif',
     miniImg: ['img/projects/foodApp/screenshot1.png', 'img/projects/foodApp/screenshot2.png', 'img/projects/foodApp/screenshot3.png', 'img/projects/foodApp/screenshot4.png', 'img/projects/foodApp/screenshot5.png'],
     technologies: ['Ruby', 'Ruby on Rails', 'Auth'],
     linkToLiveVersion: 'https://mv-food-recipe-app.herokuapp.com/',
